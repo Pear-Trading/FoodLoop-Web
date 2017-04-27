6f084171dd15c407ee3864c3454ffb98bc720d90
piratefinn <toyou1995@gmail.com>
2017-04-27 15:29:31 +0100

    Initial CoreUI commit
    The initial new codebase from a template

--- services/uploadReceiptService.js
+++
@@ -1,6 +0,0 @@
-/* this service handles receipt upload process */
-app.service('uploadReceiptService', function () {
-    this.uploadReceipt = function(){
-      console.log("Upload successful");
-    }
-});
