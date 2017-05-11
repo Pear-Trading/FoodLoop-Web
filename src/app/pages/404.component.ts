fd810c127936cd5158ef594351d2edec7fc88c1e
piratefinn <toyou1995@gmail.com>
2017-05-11 16:38:19 +0100

    Removed search on 404 & 500 replaced with page return

--- src/app/pages/404.component.ts
+++ src/app/pages/404.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   templateUrl: '404.component.html'
 })
 export class P404Component {
 
-  constructor() { }
+  constructor(
+	private location: Location
+ ) {
+	 }
 
+	 goBack(): void {
+  this.location.back();
+} 
 }
