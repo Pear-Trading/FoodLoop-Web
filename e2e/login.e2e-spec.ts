9c5c309ef06cf294681e22edad077d1ebad87e3c
Tom Bloor <t.bloor@shadowcat.co.uk>
2017-08-25 12:17:15 +0100

    Updated login page tests to check for username and password fields

--- e2e/login.e2e-spec.ts
+++ e2e/login.e2e-spec.ts
@@ -5,10 +5,20 @@ describe('Login Page', () => {
 
   beforeEach(() => {
     page = new LoginPageObject();
+    page.navigateTo();
   });
 
   it('should have a header saying login', () => {
-    page.navigateTo();
     expect(page.getLoginHeaderText()).toEqual('Login');
+  });
+
+  it('should have a username box of type text', () => {
+    expect(page.isUsernameFieldPresent()).toBeTruthy();
+    expect(page.getUsernameFieldType()).toBe('text');
+  });
+
+  it('should have a password box of type password', () => {
+    expect(page.isPasswordFieldPresent()).toBeTruthy();
+    expect(page.getPasswordFieldType()).toBe('password');
   });
 });
--- e2e/login.po.ts
+++ e2e/login.po.ts
@@ -2,10 +2,19 @@ import { browser, element, by } from 'protractor';
 
 export class LoginPageObject {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/login');
   }
 
   getLoginHeaderText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getUsernameField() { return element(by.id('username')); }
+  getPasswordField() { return element(by.id('password')); }
+
+  isUsernameFieldPresent() { return this.getUsernameField().isPresent(); }
+  isPasswordFieldPresent() { return this.getPasswordField().isPresent(); }
+
+  getUsernameFieldType() { return this.getUsernameField().getAttribute('type'); }
+  getPasswordFieldType() { return this.getPasswordField().getAttribute('type'); }
 }
