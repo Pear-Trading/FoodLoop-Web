f0ed1e9369a2ef464bcc08cbc2b59918dff68910
Tom Bloor <t.bloor@shadowcat.co.uk>
2017-09-07 16:02:29 +0100

    Update snippets to use new snippets api and dataset

--- src/app/snippets/org-snippet-bar.component.ts
+++ src/app/snippets/org-snippet-bar.component.ts
@@ -7,10 +7,19 @@ import { OrgSnippetsService } from '../providers/org-snippets.service';
 })
 export class OrgBarSnippetComponent implements OnInit {
 
-  public customersThisMonth = 0;
-  public moneySpentThisMonth = 0;
-  public pointsTotal = 0;
-  public averageTransactionToday = 0;
+  public thisMonthSalesCount = 0;
+  public thisMonthSalesTotal = 0;
+  public thisWeekSalesCount = 0;
+  public thisWeekSalesTotal = 0;
+  public todaySalesCount = 0;
+  public todaySalesTotal = 0;
+
+  public thisMonthPurchasesCount = 0;
+  public thisMonthPurchasesTotal = 0;
+  public thisWeekPurchasesCount = 0;
+  public thisWeekPurchasesTotal = 0;
+  public todayPurchasesCount = 0;
+  public todayPurchasesTotal = 0;
 
   constructor(
     private snippetsService: OrgSnippetsService,
@@ -20,10 +29,19 @@ export class OrgBarSnippetComponent implements OnInit {
     this.snippetsService.getData()
       .subscribe(
         result => {
-          this.customersThisMonth = result.snippets.this_month_customer_count;
-          this.moneySpentThisMonth = result.snippets.this_month_customer_spend;
-          this.pointsTotal = result.snippets.pear_points_total;
-          this.averageTransactionToday = result.snippets.today_sales_average;
+          this.thisMonthSalesCount = result.snippets.this_month_sales_count;
+          this.thisMonthSalesTotal = result.snippets.this_month_sales_total;
+          this.thisWeekSalesCount  = result.snippets.this_week_sales_count;
+          this.thisWeekSalesTotal  = result.snippets.this_week_sales_total;
+          this.todaySalesCount     = result.snippets.today_sales_count;
+          this.todaySalesTotal     = result.snippets.today_sales_total;
+
+          this.thisMonthPurchasesCount = result.snippets.this_week_purchases_count;
+          this.thisMonthPurchasesTotal = result.snippets.this_week_purchases_total;
+          this.thisWeekPurchasesCount  = result.snippets.this_month_purchases_count;
+          this.thisWeekPurchasesTotal  = result.snippets.this_month_purchases_total;
+          this.todayPurchasesCount     = result.snippets.today_purchases_count;
+          this.todayPurchasesTotal     = result.snippets.today_purchases_total;
         }
       );
   }
