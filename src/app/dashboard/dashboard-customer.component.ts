a2ad5dc1a0622f9f2b0bd53bc835eb854a35afa0
piratefinn <toyou1995@gmail.com>
2017-12-12 15:10:44 +0000

    More placeholder code for customer dashboard

--- src/app/dashboard/dashboard-customer.component.ts
+++ src/app/dashboard/dashboard-customer.component.ts
@@ -2,6 +2,8 @@ import { Directive, Component, OnInit } from '@angular/core';
 import { ApiService } from '../providers/api-service';
 import { Router } from '@angular/router';
 import { GraphWidget } from '../widgets/graph-widget.component';
+import { CustBarSnippetComponent } from '../snippets/cust-snippet-bar.component';
+import { DataType } from '../shared/data-types.enum';
 
 @Component({
   templateUrl: 'dashboard-customer.component.html'
@@ -20,32 +22,62 @@ export class DashboardCustomerComponent implements OnInit {
   myRank: any;
   username: any;
 
-  basicStats = {
-    today_sum: 0,
-    today_count: 0,
-    week_sum: 0,
-    week_count: 0,
-    month_sum: 0,
-    month_count: 0,
-    user_sum: 0,
-    user_count: 0,
-    global_sum: 0,
-    global_count: 0,
-    user_position: 0,
-  };
+  public widgetList = [
+    {
+      type: 'graph',
+      name: 'total_today',
+      title: 'Total Today',
+      dataType: DataType.currency,
+    },
+    {
+      type: 'graph',
+      name: 'avg_spend_today',
+      title: 'Avg. Spend Today',
+      dataType: DataType.currency,
+    },
+    {
+      type: 'graph',
+      name: 'total_last_week',
+      icon: 'icon-diamond',
+      title: 'Last Week Total',
+      dataType: DataType.currency,
+    },
+    {
+      type: 'graph',
+      name: 'avg_spend_last_week',
+      icon: 'icon-diamond',
+      title: 'Last Week Avg. Spend',
+      dataType: DataType.currency,
+    },
+    {
+      type: 'graph',
+      name: 'total_last_month',
+      title: 'Last Month Total',
+      dataType: DataType.currency,
+    },
+    {
+      type: 'graph',
+      name: 'avg_spend_last_month',
+      title: 'Last Month Avg. Spend',
+      dataType: DataType.currency,
+    },
+    {
+      type: 'graph',
+      name: 'total_user',
+      title: 'User Total',
+      dataType: DataType.currency,
+    },
+    {
+      type: 'graph',
+      name: 'avg_spend_user',
+      title: 'User Avg. Spend',
+      dataType: DataType.currency,
+    },
+  ];
 
   constructor(
   private api: ApiService,
   ) {
-    this.api.basicStats().subscribe(
-      result => {
-        this.basicStats = result;
-      },
-      error => {
-        console.log('Retrieval Error');
-        console.log( error._body );
-      }
-    );
   }
 
   ngOnInit(): void {
---
+++ src/app/providers/cust-snippets.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api-service';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class CustSnippetsService {
+  private orgSnippetsUrl = '/v1/organisation/snippets';
+
+  constructor(private api: ApiService) { }
+
+  // This endpoint should mimic basicStats
+  public getData(): Observable<any> {
+    return this.api.post(this.orgSnippetsUrl);
+  }
+}
