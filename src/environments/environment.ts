700ee3c15580ec1930f27959902c9ce09210a63c
piratefinn <toyou1995@gmail.com>
2018-03-07 12:27:16 +0000

    fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,7 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://dev.peartrade.org/api',
-  mapApiKey: 'AIzaSyBhm0iaIGG0Ko5IsfZx-CpLt01YHkp4Y1w',
-  config: {
-    "API_URL": "https://pwa-workshop-api.herokuapp.com",
-    "VAPID_PUBLIC_KEY": "BMDZ6FANqsYRF9iGo3Ki0LdltGZZksgIFbgxBr_otO0H7jTFgcm3v2bGSgnVGJ5bidvLvuKStirfDNl4khVBiok"
-  }
+  mapApiKey: 'CHANGEME',
 };
